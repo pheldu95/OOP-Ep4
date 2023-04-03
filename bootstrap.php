97e56d97456ecab71c7d7de32a9cb45ac4fe8a8c
pheldu95 <phelps.duncan@gmail.com>
2023-04-03 12:59:02 -0500

    added composer to project so we can use it's autoloader instead of the one we coded. created the composer.json file and then ran 'php composer.phar insteall

--- bootstrap.php
+++ bootstrap.php
@@ -7,11 +7,5 @@ $configuration = array(
 );
 
 //autoloader. will only load BattleManager if we call for it. where as using require statement would load it no matter what even if we aren't using it. makes app faster
-spl_autoload_register(function($className){
-    //uses the namespace to find the file. since the namespace is the same as the folder it's in
-    $path = __DIR__.'/lib/'.str_replace('\\', '/', $className).'.php';
-
-    if(file_exists($path)){
-        require $path;
-    }
-});
+//now we are using the composer autoloader.
+require __DIR__.'/vendor/autoload.php';
